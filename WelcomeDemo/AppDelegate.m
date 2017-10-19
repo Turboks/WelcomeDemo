4ea99b9447da070216415870ad68eb9422dc2736
dylan <342409840@qq.com>
2017-10-19 11:58:25 +0800

    two

--- WelcomeDemo/AppDelegate.m
+++ WelcomeDemo/AppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import "OnboardingViewController.h"
+#import "OnboardingContentViewController.h"
+
+static NSString * const kUserHasOnboardedKey = @"user_has_onboarded";
 
 @interface AppDelegate ()
 
@@ -16,11 +20,72 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    BOOL userHasOnboarded = [[NSUserDefaults standardUserDefaults] boolForKey:kUserHasOnboardedKey];
+    if(userHasOnboarded){
+        [self setupNormalRootViewController];
+    }else{
+        self.window.rootViewController = [self generateMovieOnboardingVC];
+    }
+    
+    application.statusBarStyle = UIStatusBarStyleLightContent;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
-
-
+//直接进入app内部
+-(void)setupNormalRootViewController{
+    UIViewController *mainVC = [UIViewController new];
+    mainVC.title = @"Main Application";
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:mainVC];
+}
+//加载视频样式的欢迎页
+- (OnboardingViewController *)generateMovieOnboardingVC {
+    OnboardingContentViewController *firstPage = [[OnboardingContentViewController alloc] initWithTitle:@"标题一" body:@".内容一." image:nil buttonText:nil action:nil];
+    firstPage.topPadding = -15;
+    firstPage.underTitlePadding = 160;
+    firstPage.titleLabel.textColor = [UIColor whiteColor];
+    firstPage.titleLabel.font = [UIFont fontWithName:@"ArialMT" size:60.0];
+    firstPage.bodyLabel.textColor = [UIColor whiteColor];
+    firstPage.bodyLabel.font = [UIFont fontWithName:@"ArialMT" size:30.0];
+    
+    OnboardingContentViewController *secondPage = [[OnboardingContentViewController alloc] initWithTitle:@"标题二" body:@".内容二." image:nil buttonText:nil action:nil];
+    secondPage.titleLabel.font = [UIFont fontWithName:@"ArialMT" size:38.0];
+    secondPage.underTitlePadding = 170;
+    secondPage.topPadding = 0;
+    secondPage.titleLabel.textColor = [UIColor colorWithRed:251/255.0 green:176/255.0 blue:59/255.0 alpha:1.0];
+    secondPage.bodyLabel.textColor = [UIColor colorWithRed:251/255.0 green:176/255.0 blue:59/255.0 alpha:1.0];
+    secondPage.bodyLabel.font = [UIFont fontWithName:@"ArialMT" size:18.0];
+    
+    OnboardingContentViewController *thirdPage = [[OnboardingContentViewController alloc] initWithTitle:@"welcome" body:@".内容三." image:nil buttonText:@"进入应用" action:^{
+        [self handleOnboardingCompletion];
+    }];
+    thirdPage.topPadding = 10;
+    thirdPage.underTitlePadding = 160;
+    thirdPage.bottomPadding = -10;
+    thirdPage.titleLabel.font = [UIFont fontWithName:@"ArialMT" size:38.0];
+    thirdPage.titleLabel.textColor = [UIColor colorWithRed:58/255.0 green:105/255.0 blue:136/255.0 alpha:1.0];
+    thirdPage.bodyLabel.textColor = [UIColor colorWithRed:58/255.0 green:105/255.0 blue:136/255.0 alpha:1.0];
+    thirdPage.bodyLabel.font = [UIFont fontWithName:@"ArialMT" size:15.0];
+    [thirdPage.actionButton setTitleColor:[UIColor colorWithRed:239/255.0 green:88/255.0 blue:35/255.0 alpha:1.0] forState:UIControlStateNormal];
+    thirdPage.actionButton.titleLabel.font = [UIFont fontWithName:@"ArialMT" size:17.0];
+    
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *moviePath = [bundle pathForResource:@"demo" ofType:@"mp4"];
+    NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
+    
+    OnboardingViewController *onboardingVC = [[OnboardingViewController alloc] initWithBackgroundVideoURL:movieURL contents:@[firstPage, secondPage, thirdPage]];
+    onboardingVC.shouldFadeTransitions = YES;
+    onboardingVC.shouldMaskBackground = NO;
+    onboardingVC.pageControl.currentPageIndicatorTintColor = [UIColor colorWithRed:239/255.0 green:88/255.0 blue:35/255.0 alpha:1.0];
+    onboardingVC.pageControl.pageIndicatorTintColor = [UIColor whiteColor];
+    return onboardingVC;
+}
+- (void)handleOnboardingCompletion {
+    [self setupNormalRootViewController];
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
